aaa688a0ef18d0f433d03b820d64b60bb6a43a5a
liuzhenjun <1182799153@qq.com>
2016-05-27 08:43:24 +0800

    版本1.2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-$("document").ready(function(){
+//$("document").ready(function(){
 	var index = 0;
 	var l = $(".skSliderimg ul li").size();
 	function myshow(){
@@ -7,8 +7,7 @@ $("document").ready(function(){
 		$(".skSliderimg ul li").eq(index).find(".img2").animate({"left":"550px"},1000);
 	}
 
-	var timer = setInterval(function(){change()},2000);
-	
+	var timer = setInterval("change()",2000);
 	function change(){
 		index++;
 		if(index == l){
@@ -16,16 +15,16 @@ $("document").ready(function(){
 		}
 		$(".skSliderimg ul li").eq(index).fadeIn(100).siblings().fadeOut(100);
 		$(".skSlidernav ul li").eq(index).addClass("select").siblings().removeClass("select");
-
+	
 		myshow();
-
+	
 		$(".skSliderimg ul li").eq(index).siblings().find("img1").css("left","-20px");
 		$(".skSliderimg ul li").eq(index).siblings().find(".img2").css("left","600px");
-
+	
 		var color=$(".skSliderimg ul li").eq(index).css("background-color");
-
+	
 		$(".contanier_banner").css("background-color",color);
-
+	
 	};
 	
 	$(".skSlidernav ul li").mouseover(function(){
@@ -36,50 +35,104 @@ $("document").ready(function(){
 		console.log(navLis);
 		$(".skSliderimg ul li").eq($(this).index()).fadeIn(100).siblings().fadeOut(100);
 		$(".skSlidernav ul li").eq(navLis).addClass("select").siblings().removeClass("select");
-		timer = setInterval(function(){change()},2000);
+		timer = setInterval("change()",2000);
 		index=navLis;
 		return false;
 	})
 	
 	$(".skSliderimg").mouseover(function(){
+		
 		console.log("come baby");
 		$(".skSlider_btnleft,.skSlider_btnright").css("display", "block");
 		return false;
 	})
 	$(".skSliderimg").mouseout(function(){
+		
 		console.log("come baby");
 		$(".skSlider_btnleft,.skSlider_btnright").css("display", "none");
 		return false;
 	})
-
+	//上一页下一页 切换代码
 	$(".skSlider_btnleft").mouseover(function(){
+		
 		$(".skSlider_btnleft").css("opacity", "1");
 		
 	})
 	$(".skSlider_btnleft").mouseout(function(){
+		
 		$(".skSlider_btnleft").css("opacity", ".5");
 		
 	})
 	$(".skSlider_btnright").mouseover(function(){
+		
 		$(".skSlider_btnright").css("opacity", "1");
 		
 	})
 	$(".skSlider_btnright").mouseout(function(){
+		
 		$(".skSlider_btnright").css("opacity", ".5");
 		
 	})
-	$(".skSlider_btnleft").mousedown(function(){
+	$(".skSlider_btnleft").click(function(){
+		
 		clearInterval(timer);
 		$(".skSliderimg ul li").eq(index-1).fadeIn(100).siblings().fadeOut(100);
 		$(".skSlidernav ul li").eq(index-1).addClass("select").siblings().removeClass("select");
-		index-=1;
-		timer = setInterval(function(){change()},2000);
+		index-=2;
+		if(index < 0){
+			index = 4;
+		}
+		change();
+		timer = setInterval("change()",2000);
 	})
-	$(".skSlider_btnright").mousedown(function(){
+	$(".skSlider_btnright").click(function(){
+		
 		clearInterval(timer);
 		$(".skSliderimg ul li").eq(index+1).fadeIn(100).siblings().fadeOut(100);
 		$(".skSlidernav ul li").eq(index+1).addClass("select").siblings().removeClass("select");
-		index+=1;
-		timer = setInterval(function(){change()},2000);
+		
+		if(index == l){
+			index = 0;
+		}
+		change();
+		timer = setInterval("change()",2000);
 	})
-})
+	
+	//slideBar中的js代码
+	$(".webf1").hover(
+		function(){
+			$(".webf1 dt").eq($(this).index()).addClass("webf1_js_bg");
+			$(".webf1 dt strong").eq($(this).index()).addClass("webf1_js_color_strong");
+			$(".webf1 dt a").eq($(this).index()).addClass("webf1_js_color_a");
+			$(".webf1 dt a:hover").eq($(this).index()).css("color", "red");
+			$(".webf1 dd").eq($(this).index()).css("display", "block");
+			$(".webf1 dd").eq($(this).index()).css("top", -($(this).index()*40)+"px");
+	},
+		function(){
+			$(".webf1 dt").eq($(this).index()).removeClass("webf1_js_bg");
+			$(".webf1 dt strong").eq($(this).index()).removeClass("webf1_js_color_strong");
+			$(".webf1 dt a").eq($(this).index()).removeClass("webf1_js_color_a");
+			$(".webf1 dt a:hover").eq($(this).index()).css("color", "red");
+			$(".webf1 dd").eq($(this).index()).css("display", "none");
+		}
+	)
+	/*$(".webf1").mouseover(function(){
+		
+		$(".webf1 dt").eq($(this).index()).addClass("webf1_js_bg");
+		$(".webf1 dt strong").eq($(this).index()).addClass("webf1_js_color_strong");
+		$(".webf1 dt a").eq($(this).index()).addClass("webf1_js_color_a");
+		$(".webf1 dt a:hover").eq($(this).index()).css("color", "red");
+		$(".webf1 dd").eq($(this).index()).css("display", "block");
+		$(".webf1 dd").eq($(this).index()).css("top", -($(this).index()*40)+"px");
+	})
+	$(".webf1").mouseout(function(){
+		
+		$(".webf1 dt").eq($(this).index()).removeClass("webf1_js_bg");
+		$(".webf1 dt strong").eq($(this).index()).removeClass("webf1_js_color_strong");
+		$(".webf1 dt a").eq($(this).index()).removeClass("webf1_js_color_a");
+		$(".webf1 dt a:hover").eq($(this).index()).css("color", "red");
+		$(".webf1 dd").eq($(this).index()).css("display", "none");
+	})*/
+//})
+
+
